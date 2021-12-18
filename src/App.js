e2b90e32f6f4dbaedfe40ec3e50b1e5fa399bae8
Swanand-Wagh <swanandwagh7@gmail.com>
2021-12-19 00:23:17 +0530

    fetched states from api & displayed in dropdown

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
+import "./App.css";
+import InfoBox from "./components/InfoBox";
 
 const App = () => {
-  return <div className="App">Hello</div>;
+  const [state, setState] = useState("India");
+  const [allStates, setAllStates] = useState([]);
+  const [mapStates, setMapStates] = useState([]);
+
+  useEffect(() => {
+    const getStatesData = async () => {
+      fetch("https://www.mohfw.gov.in/data/datanew.json")
+        .then((response) => response.json())
+        .then((data) => {
+          const states = data.map((st) => ({
+            name: st.state_name,
+            value: st.state_name,
+          }));
+          setAllStates(states);
+          setMapStates(data);
+        });
+    };
+
+    getStatesData();
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 Tracker</h1>
+          <FormControl className="app__dropdown">
+            <Select variant="outlined">
+              <MenuItem value="india">India</MenuItem>
+              {allStates.map((st) => (
+                <MenuItem value={st.value}>{st.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+      </div>
+    </div>
+  );
 };
 
 export default App;
