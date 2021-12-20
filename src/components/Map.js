5e270727636fe2ecc38b397577c0f1fc634046bc
Swanand-Wagh <swanandwagh7@gmail.com>
2021-12-20 13:29:46 +0530

    change map data language to english

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,14 +2,11 @@ import React from "react";
 import { Map as LeafletMap, TileLayer } from "react-leaflet";
 import "./Map.css";
 
-function Map({ countries, casesType, center, zoom }) {
+function Map({ states, casesType, center, zoom }) {
   return (
     <div className="map">
       <LeafletMap center={center} zoom={zoom}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        />
+        <TileLayer url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png" />
       </LeafletMap>
     </div>
   );
